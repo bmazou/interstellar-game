0296c67954deb64b431b1d6fca96ee43792a7aa5
Jakub Horsky <jh6220@ic.ac.uk>
2022-09-03 17:00:05 +0200

    player control

--- escape-life/Assets/PlayerControl.cs
+++ escape-life/Assets/PlayerControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float velocityScaler = 1;
+
     private Rigidbody2D rb;
 
     void Start()
@@ -14,18 +16,26 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            rb.mass++;
-        }
-        if(Input.GetMouseButtonDown(1))
-        {
-            rb.mass--;
-        }
 
         if (Input.GetKeyDown("w"))
         {
-            rb.AddForce()
+            rb.velocity = new Vector2(0,velocityScaler);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            rb.velocity = new Vector2(0,-velocityScaler);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            rb.velocity = new Vector2(-velocityScaler, 0);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            rb.velocity = new Vector2(velocityScaler, 0);
+        }
+        if (Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("s") || Input.GetKeyUp("d"))
+        {
+            rb.velocity = Vector2.zero;
         }
     }
 }
